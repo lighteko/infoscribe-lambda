022bea32b54601f114b5c13e787b068370824323
lighteko <dev.lighteko@gmail.com>
2025-01-27 00:23:15 -0500

    test

--- lib/news/gnews.py
+++ lib/news/gnews.py
@@ -6,7 +6,7 @@ import logging
 
 
 class GNews:
-    API_KEY: str
+    API_KEY: str = ""
 
     def __init__(self):
         pass
@@ -16,11 +16,14 @@ class GNews:
         cls.API_KEY = app.config["GNEWS_API_KEY"]
 
     def get_news(self, lang, country, topic: str):
+        if GNews.API_KEY is "": 
+            logging.error("G News API KEY not initialized")
         response = requests.get(
             f"https://gnews.io/api/v4/search?q={topic}&lang={lang}&country={country}&max=10&apikey={GNews.API_KEY}")
         news_list = []
+        res = response.json()
         news_link_list = [article["url"]
-                          for article in response.json()["articles"]]
+                          for article in res["articles"]]
         for news in news_link_list:
             article = NewsPlease.from_url(news)
             try:
