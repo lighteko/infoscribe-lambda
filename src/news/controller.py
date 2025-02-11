d4c8889d4e69a2779bcb3c20fb7f9654d1195d63
lighteko <dev.lighteko@gmail.com>
2025-02-11 02:50:36 -0500

    EDIT: Modified to parse request from EventBridge

--- src/news/controller.py
+++ src/news/controller.py
@@ -1,25 +1,41 @@
-from flask.views import MethodView
-from flask import request, abort
+import json
 from src.news.service import NewsService
 from src.make_output import make_output
 
 
-class NewsController(MethodView):
+class Controller:
     def __init__(self):
         self.service = NewsService()
 
-    def get(self):
-        lang = request.args.get("lang", default=None, type=str)
-        country = request.args.get("country", default=None, type=str)
-        level = request.args.get("level", default=None, type=str)
-        keywords = request.args.get(
-            "keywords", default=None, type=str)
+    def get(self, event):
+        try:
+            if "detail" in event:
+                message = event["detail"]
+            else:
+                return make_output(data={}, error=400, status="invalid event structure")
 
-        if lang == None or country == None or level == None or keywords == None:
-            abort(400, description="Arguments are not fully provided")
+            event_type = message.get("eventType")
 
-        keywords = keywords.split(", ")
+            if not event_type:
+                return make_output(data={}, error=400, status="missing event type")
 
-        response = self.service.make_newsletter(lang, country, keywords, level)
+            provider_id = message.get("providerId")
+            locale = message.get("locale")
+            categories = message.get("categories")
 
-        return make_output(data=response)
+            if not provider_id or not locale:
+                return make_output(data={}, error=400, status="missing required fields")
+
+            if event_type == "collect":
+                self.service.daily_summarize(locale, categories)
+            elif event_type == "build":
+                self.service.make_newsletter(locale, categories)
+            else:
+                return make_output(data={}, error=400, status="invalid event type")
+
+        except json.JSONDecodeError:
+            return make_output(data={}, error=400, status="invalid JSON format")
+        except KeyError:
+            return make_output(data={}, error=400, status="missing necessary fields")
+        except Exception as e:
+            return make_output(data={}, error=500, status="internal server error", message=str(e))
